f0ec8671430af1999b2d6e3cd5d57ee82378dd36
marko <markopolo0790@mail.ru>
2024-05-23 21:14:18 +0300

    create mainFragment, add onclick for button

--- app/src/main/java/com/example/interactiveforchield/MainActivity.kt
+++ app/src/main/java/com/example/interactiveforchield/MainActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.interactiveforchield.constant.MAIN
+import com.example.interactiveforchield.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+
+        MAIN = this
 
     }
 }
---
+++ app/src/main/java/com/example/interactiveforchield/constant/constants.kt
@@ -0,0 +1,5 @@
+package com.example.interactiveforchield.constant
+
+import com.example.interactiveforchield.MainActivity
+
+lateinit var MAIN:MainActivity
